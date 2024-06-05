4edbe3fe301500a07ea00283889eb3c52e0ba7ca
INFOR\matteou <matteourso100@gmail.com>
2024-06-05 14:19:04 +0200

    Navbar
    Create MobileNav with sheet and Navbar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,11 +11,11 @@ function Sidebar() {
   const pathname = usePathname();
 
   return (
-    <section className="lg-w-[264px] sticky left-0 top-0 h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden">
-      <div className="flex flex-col gap-6">
+    <section className="sticky left-0 top-0 h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
           const isActive =
-            pathname === link.route || pathname.startsWith(link.route);
+            pathname === link.route || pathname.startsWith(`${link.route}/`);
 
           return (
             <Link
