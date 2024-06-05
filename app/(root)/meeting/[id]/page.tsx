c409311d7baf1b2c5cd580755c5f0247d16cc69b
UfoTheKing <matteourso100@gmail.com>
2024-06-05 23:23:42 +0200

    Meeting Page

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+"use client";
+
+import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
 
 function Meeting({ params }: { params: { id: string } }) {
-  return <div>Meeting Room: #{params.id}</div>;
+  const { call, isCallLoading } = useGetCallById(params.id);
+
+  const { user, isLoaded } = useUser();
+
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  return (
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 }
 
 export default Meeting;
