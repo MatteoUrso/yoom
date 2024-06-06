864f7d6f1ca80e3d5204acda09a935122677c6e9
INFOR\matteou <matteourso100@gmail.com>
2024-06-06 10:50:14 +0200

    Join Meeting & Personal Room

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -9,6 +9,7 @@ import MeetingModal from "./MeetingModal";
 import { useUser } from "@clerk/nextjs";
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 import ReactDatePicker from "react-datepicker";
+import { Input } from "./ui/input";
 
 function MeetingTypeList() {
   const { toast } = useToast();
@@ -170,6 +171,21 @@ function MeetingTypeList() {
         buttonText="Start Meeting"
         handleClick={createMeeting}
       />
+
+      <MeetingModal
+        isOpen={meetingState === "isJoiningMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Type the link here"
+        className="text-center"
+        buttonText="Join Meeting"
+        handleClick={() => router.push(`/meeting/${values.link}`)}
+      >
+        <Input
+          placeholder="Meeting link"
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+          className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+        />
+      </MeetingModal>
     </section>
   );
 }
